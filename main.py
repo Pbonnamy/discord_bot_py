2d470fe4396d591259c72b97a8ca8a2481e2b34c
Pierre <bonnamy.77@gmail.com>
2022-01-20 20:53:48 +0100

    hello world discord ver

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello world')
+import discord
+import os
+from dotenv import load_dotenv
+
+client = discord.Client()
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+LOG_CHANNEL = int(os.getenv('LOG_CHANNEL'))
+CHANNEL = int(os.getenv('CHANNEL'))
+
+
+@client.event
+async def on_ready():
+    await client.get_channel(LOG_CHANNEL).send(client.user.name + ' is **online**')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.channel.id == CHANNEL:
+        if message.content == '!hello':
+            await message.channel.send('Hello <@' + str(message.author.id) + '> !')
+
+client.run(TOKEN)
