7b2a08c9d22f166413bc93fcdd5c722c31063c80
Pierre <bonnamy.77@gmail.com>
2022-02-24 22:33:26 +0100

    with json and reactions

--- guess_champion.py
+++ guess_champion.py
@@ -2,12 +2,16 @@ import discord
 import settings
 import asyncio
 import time
+import json
+import random
 
 timeout = 10
 
 
 async def guess(client):
-    file = discord.File("assets/pixelated/xerath.jpg", filename="xerath.jpg")
+    champion = pick_champion()
+
+    file = discord.File("assets/pixelated/"+champion['image'], filename=champion['image'])  # pixéliser 20
 
     embed = discord.Embed(
         title='Who\'s that champion ?',
@@ -15,17 +19,32 @@ async def guess(client):
     )
 
     embed.set_image(
-        url="attachment://xerath.jpg"
+        url="attachment://"+champion['image']
     )
 
     await client.get_channel(settings.CHANNEL).send(file=file, embed=embed)
 
     end = time.time() + timeout
+    win = False
 
     while time.time() < end:
         try:
-            msg = await client.wait_for("message", check=None, timeout=end-time.time())
+            msg = await client.wait_for("message", check=None, timeout=end - time.time())
+
+            if msg.content == champion['name']:
+                await msg.add_reaction('✅')
+                await client.get_channel(settings.CHANNEL).send('**Bien joué, vous gagnez ' + str(champion['points']) + ' points !**')
+                win = True
+            else:
+                await msg.add_reaction('❌')
         except asyncio.TimeoutError:
             break
 
-    await client.get_channel(settings.CHANNEL).send('**Le jeu est fini, vous n\'avez pas trouvé !**')
+    if not win:
+        await client.get_channel(settings.CHANNEL).send('**Le jeu est fini, vous n\'avez pas trouvé !**')
+
+
+def pick_champion():
+    with open("champions.json") as file:
+        content = json.load(file)
+        return content[random.randint(0, len(content)-1)]
